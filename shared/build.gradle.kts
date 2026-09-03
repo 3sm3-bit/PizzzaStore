168ef6ae15a42c63f1dc18f51581ee202d69ab8e
Tayler <emontesinos812@gmail.com>
2026-09-02 22:23:24 -0500

    update url

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -19,8 +19,8 @@ buildConfig {
     val isDebug = isAndroidDebug || isIosDebug || project.hasProperty("debug")
     
     buildConfigField("Boolean", "IS_DEBUG", isDebug.toString())
-    buildConfigField("String", "BASE_URL_SERVICE", "\"https://undefined-facility-salute.ngrok-free.dev\"")
-    buildConfigField("String", "BASE_URL_SERVICE_DEV", "\"https://undefined-facility-salute.ngrok-free.dev\"")
+    buildConfigField("String", "BASE_URL_SERVICE", "\"https://app.lapizzzeria.lol\"")
+    buildConfigField("String", "BASE_URL_SERVICE_DEV", "\"https://app.lapizzzeria.lol\"")
 }
 
 kotlin {
